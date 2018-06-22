51b213de800d04fd7bd7393be6d4eecd6a6c4bc2
LeeSangHoon <hun6728@ridi.com>
2018-06-22 15:26:10 +0900

    패키지명 변경 & 쓸모없는 `.gitignore` 삭제

---
+++ app/src/main/java/com/ridi/demoapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.ridi.demoapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Button
+import com.ridi.oauth2.RidiOAuth2
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.loginButton).setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.getAuthTokenButton).setOnClickListener {
+            Thread().run {
+                RidiOAuth2.setDev()
+                RidiOAuth2.setTokenFilePath(Environment.getExternalStorageDirectory().absolutePath + "/tokenJSON.json")
+                RidiOAuth2.setClientId("Nkt2Xdc0zMuWmye6MSkYgqCh9q6JjeMCsUiH1kgL")
+                RidiOAuth2.getOAuthToken("app://authorized").subscribe({
+                    Log.d(javaClass.name, "Received => $it")
+                }, {
+                    Log.e(javaClass.name, "Error => $it")
+                })
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/ridi/demoapp/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.ridi.demoapp
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import java.util.ArrayList
+
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val permissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onPermissionDenied(deniedPermissions: ArrayList<String>) {
+                Toast.makeText(this@SplashActivity, "권한을 설정하셔야 이용하실 수 있습니다.", Toast.LENGTH_SHORT)
+                    .show()
+                finish()
+            }
+        }
+
+        TedPermission.with(this@SplashActivity)
+            .setPermissionListener(permissionListener)
+            .setDeniedMessage("권한을 설정하지 않으시면 서비스를 이용하실 수 없습니다")
+            .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .check()
+    }
+}
