f9747d08dbf3b2f9d99a21217cc49de3f2aad120
kering <kering@ridi.com>
2018-09-07 15:10:59 +0900

    Improve encryption with randomized initial vectors.

--- library/src/main/kotlin/com/ridi/oauth2/EncryptionUtil.kt
+++ library/src/main/kotlin/com/ridi/oauth2/EncryptionUtil.kt
@@ -1,31 +1,31 @@
 package com.ridi.oauth2
 
 import android.util.Base64
-import android.util.Base64.encodeToString
+import java.security.SecureRandom
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
-private val ivSpec = IvParameterSpec(ByteArray(16))
+private const val TRANSFORMATION = "AES/CBC/PKCS5Padding"
+private const val IV_SIZE = 16
+
+private fun String.toSecretKeySpec() = SecretKeySpec(toByteArray(), "AES")
 
 internal fun String.encodeWithAES128(key: String?): String {
-    if (key == null) {
-        return this
-    }
+    key ?: return this
 
-    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
-    val secretKeySpec = SecretKeySpec(key.toByteArray(Charsets.UTF_8), "AES")
-    cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivSpec)
-    return encodeToString(cipher.doFinal(this.toByteArray(Charsets.UTF_8)), 0)
+    val cipher = Cipher.getInstance(TRANSFORMATION)
+    val iv = ByteArray(IV_SIZE)
+    SecureRandom().nextBytes(iv)
+    cipher.init(Cipher.ENCRYPT_MODE, key.toSecretKeySpec(), IvParameterSpec(iv))
+    return Base64.encodeToString(iv + cipher.doFinal(toByteArray()), 0)
 }
 
 internal fun String.decodeWithAES128(key: String?): String {
-    if (key == null) {
-        return this
-    }
+    key ?: return this
 
-    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
-    val secretKeySpec = SecretKeySpec(key.toByteArray(Charsets.UTF_8), "AES")
-    cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivSpec)
-    return String(cipher.doFinal(Base64.decode(this, 0)), Charsets.UTF_8)
+    val byteArray = Base64.decode(this, 0)
+    val cipher = Cipher.getInstance(TRANSFORMATION)
+    cipher.init(Cipher.DECRYPT_MODE, key.toSecretKeySpec(), IvParameterSpec(byteArray, 0, IV_SIZE))
+    return String(cipher.doFinal(byteArray, IV_SIZE, byteArray.size - IV_SIZE))
 }
