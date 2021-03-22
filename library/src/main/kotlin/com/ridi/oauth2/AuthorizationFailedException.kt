880eb6dc5f886baae438c200300868bdfaae9419
YSYoon <ys.yoon@ridi.com>
2021-03-22 13:48:38 +0900

    Add default error description

--- library/src/main/kotlin/com/ridi/oauth2/AuthorizationFailedException.kt
+++ library/src/main/kotlin/com/ridi/oauth2/AuthorizationFailedException.kt
@@ -3,5 +3,5 @@ package com.ridi.oauth2
 class AuthorizationFailedException(
     val httpStatusCode: Int,
     val errorCode: String? = null,
-    errorDescription: String? = null
+    errorDescription: String? = "status=$httpStatusCode errorCode=$errorCode"
 ) : RuntimeException(errorDescription)
